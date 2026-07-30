a91ff696621d4df553d08e67fee684538988ba82
abhishek-217 <abhiman5196@gmail.com>
2026-07-30 10:37:15 +0530

    Fix api call

--- QueueSphere-backend/src/main/java/com/healthcare/queue/config/SecurityConfig.java
+++ QueueSphere-backend/src/main/java/com/healthcare/queue/config/SecurityConfig.java
@@ -52,7 +52,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:*"));
+        configuration.setAllowedOriginPatterns(List.of(
+                "http://localhost:*",
+                "https://queuesphere-frontend.vercel.app/"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
